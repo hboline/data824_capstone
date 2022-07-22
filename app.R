8ca3b532b664ac7fb20a358af8f4486d92d047cc
hboline <hnboline@gmail.com>
2022-07-22 11:29:55 -0500

    interactive visualization complete

--- app.R
+++ app.R
@@ -59,7 +59,10 @@ ui <- dashboardPage(skin = "green",
           column(width = 3,
                  
             box(width = 10, title = "Variable Options",
-              selectInput(inputId = "xvar", label = "Select x-variable:", choices = var.names[2:8]),
+              selectInput(inputId = "xvar", label = "Select x-variable:", 
+                          choices = append(var.names[9], var.names[2:8]), selected = "height"),
+              selectInput(inputId = "yvar", label = "Select y-variable:", 
+                          choices = append(var.names[9], var.names[2:8]), selected = "age"),
               checkboxGroupInput(inputId = "sexes", label = "Select genders:", 
                                  choiceNames = c("Male", "Female", "Infant"),
                                  choiceValues = c("male", "female", "infant"),
@@ -68,7 +71,8 @@ ui <- dashboardPage(skin = "green",
             box(width = 10, title = "Plot Options",
               sliderInput(inputId = "alpha", label = "Point opacity:",
                           min = 0.0, max = 1.0, step = 0.1, value = 1.0),
-              checkboxInput(inputId = "jitchk", label = "Jitter points?", value = TRUE)
+              checkboxInput(inputId = "jitchk", label = "Jitter points?", value = TRUE),
+              checkboxInput(inputId = "matchfill", label = "Match fill color?", value = FALSE)
             )
           ),
           
@@ -98,18 +102,36 @@ server <- function(input, output, session) {
   
   output$plot1 <- renderPlotly({
     xvar <- input$xvar
+    yvar <- input$yvar
+    xvar_name <- names(which(var.names == xvar))
+    yvar_name <- names(which(var.names == yvar))
     sexes <- input$sexes
-    data.in <- data %>% select("sex", eval(xvar), "age")
-    if (colnames(data.in)[2] %in% c("length", "height", "diameter")) {
-      unit = " (mm)"
+    
+    ind2 <- 2
+    ind3 <- if_else(xvar_name == yvar_name, 2, 3)
+    
+    data.in <- data %>% select("sex", eval(xvar), eval(yvar))
+    if (colnames(data.in)[ind2] %in% c("length", "height", "diameter")) {
+      unitx = " (mm)"
+    } else if (colnames(data.in)[ind2] %in% c("whole.weight", "shucked.weight", "viscera.weight", "shell.weight")){
+      unitx = " (g)"
     } else {
-      unit = " (g)"
+      unitx = " (years)"
+    }
+    if (colnames(data.in)[ind3] %in% c("length", "height", "diameter")) {
+      unity = " (mm)"
+    } else if (colnames(data.in)[ind3] %in% c("whole.weight", "shucked.weight", "viscera.weight", "shell.weight")){
+      unity = " (g)"
+    } else {
+      unity = " (years)"
     }
     #colnames(data.in)[2] <- names(which(var.names == xvar))
     
-    xlims <- c(min(data.in[,2]),max(data.in[,2]))
-    ylims <- c(min(data.in[,3]),max(data.in[,3]))
+    xlims <- c(min(data.in[,ind2]),max(data.in[,ind2]))
+    ylims <- c(min(data.in[,ind3]),max(data.in[,ind3]))
     clims <- hue_pal()(3)
+    
+    shape <- if_else(input$matchfill, 19, 21)
     
     if (length(sexes) != 0) {
       if (!"male" %in% sexes) {
@@ -123,29 +145,29 @@ server <- function(input, output, session) {
       }
       genchk <- " + Gender"
       p1 <- data.in %>% 
-        ggplot(aes(x = .[,2], y = age, color = sex,
+        ggplot(aes(x = .[,ind2], y = .[,ind3], color = sex,
                    text = paste0(
-                     names(which(var.names == xvar)), ": ", .[,2], " ", unit,
-                     "\nAge: ", age, " years",
+                     xvar_name, ": ", .[,ind2], " ", unitx,
+                     "\n", yvar_name, ": ", .[,ind3], " ", unity,
                      "\nSex:", sex
                    ))) +
-        geom_point(shape = 21, size = 2, alpha = input$alpha, fill = "white", 
+        geom_point(shape = shape, size = 2, alpha = input$alpha, fill = "white", 
                    position = position_jitter(width = input$jitchk, seed = 1))
     } else {
       p1 <- data.in %>% 
-        ggplot(aes(x = .[,2], y = age,
+        ggplot(aes(x = .[,ind2], y = .[,ind3],
                    text = paste0(
-                     names(which(var.names == xvar)), ": ", .[,2], " ", unit,
-                     "\nAge: ", age, " years"
+                     xvar_name, ": ", .[,ind2], " ", unitx,
+                     "\n", yvar_name, ": ", .[,ind3], " ", unity
                    ))) +
-        geom_point(shape = 21, size = 2, alpha = input$alpha, fill = "white", color = "gray40", 
+        geom_point(shape = shape, size = 2, alpha = input$alpha, fill = "white", color = "gray40", 
                    position = position_jitter(width = input$jitchk, seed = 1))
       genchk <- ""
     }
     
     p1 <- p1 + 
-      labs(x = paste0(names(which(var.names == xvar)), unit), y = "Age (years)", color = "Sex", 
-           title = paste0("Abalone Age ~ ", names(which(var.names == xvar)), genchk),
+      labs(x = paste0(xvar_name, unitx), y = paste0(yvar_name, unity), color = "Sex", 
+           title = paste0("Abalone ", yvar_name, " ~ ", xvar_name, genchk),
            subtitle = paste0("For n = ", nrow(data), " observations")) +
       scale_color_manual(values = list("male"   = clims[3],
                                        "female" = clims[1],
